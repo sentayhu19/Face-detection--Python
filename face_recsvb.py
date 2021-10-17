0e2ec94ce0410161bf4c91c0e32929ef51e0a3a1
sentayhu19 <sentayhuberhanu19@gmail.com>
2021-10-17 18:23:25 +0300

    trained AI reco

---
+++ face_recsvb.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 as cv
+haar_cascade =cv.CascadeClassifier('haar_face.xml')
+people = ['Angelina Jolie','Keanu Reeves','Charlize Theron','teddy afro']
+#features = np.load('features.npy')
+#lables = np.load('lables.npy')
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.read('face_trained.yml')
+img = cv.imread(r'C:\Users\SVB\Desktop\openCv\Charlize Theron\valsvb\c13.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow('person',gray)
+faces_rect= haar_cascade.detectMultiScale(gray,1.1,4)
+for (x,y,w,h) in  faces_rect:
+    faces_rol = gray[y:y+h,x:x+h]
+    label, confidence =face_recognizer.predict(faces_rol)
+    print(f'lable = {people[label]} with a confidence of {confidence}')
+
+    cv.putText(img,str(people[label]), (20,20),cv.FONT_HERSHEY_COMPLEX,1.0,(0,255,0), thickness=2)
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+    cv.imshow('Detected face ',img)
+    cv.waitKey(0)
--- faces_train.py
+++ faces_train.py
@@ -1,4 +1,36 @@
 import os
 import cv2 as cv
 import numpy as np
- people = ['Angelina Jolie','Keanu Reeves','Charlize Theron','teddy afro']
+people = ['Angelina Jolie','Keanu Reeves','Charlize Theron','teddy afro']
+print(people)
+DIR =r'C:\Users\SVB\Desktop\openCv'
+haar_cascade =cv.CascadeClassifier('haar_face.xml')
+features=[]
+labeles=[]
+def  create_train():
+    for person in people:
+        path=os.path.join(DIR,person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path,img)
+
+            img_array = cv.imread(img_path)
+            gray = cv.cvtColor(img_array,cv.COLOR_BGR2GRAY)
+            faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,
+            minNeighbors=4)
+            for (x,y,w,h ) in faces_rect:
+                faces_roi = gray[y:y+h,x:x+w]
+                features.append(faces_roi)
+                labeles.append(label)
+create_train()
+print("*** Trianing AI Done ***")
+features = np.array(features,dtype='object')
+labeles =np.array(labeles)
+#print(f'length of the features = {len(features)}')
+#print(f'length of the labels = {len(labeles)}')
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.train(features,labeles)
+face_recognizer.save('face_trained.yml')
+np.save('features.npy',features)
+np.save('lables.npy',labeles)
